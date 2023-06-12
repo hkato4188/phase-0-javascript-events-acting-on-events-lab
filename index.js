a244a2a963eccb5262896d9ded93b9713bcd6fac
Hiro Kato <hirokikato1@gmail.com>
2023-06-12 16:57:51 -0400

    updates parameters so dodger remains in game element

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const dodger = document.getElementById("dodger");
 function moveDodgerLeft(){
     const leftPos = dodger.style.left.slice(0, -2);
     const leftNum = parseInt(leftPos, 10);
-    dodger.style.left = `${leftNum - 5}px`;
+    dodger.style.left = `${leftNum > 0 ? leftNum - 5 : leftNum}px`;
 }
 
 function moveDodgerRight(){
     const rightPos = dodger.style.left.slice(0, -2);
     const rightNum = parseInt(rightPos, 10);
-    dodger.style.left = `${rightNum + 5}px`;
+    dodger.style.left = `${rightNum < 360 ? rightNum + 5 : rightNum}px`;
 }
 
 function moveDodgerUp(){
     const upPos = dodger.style.bottom.slice(0, -2);
     const upNum = parseInt(upPos, 10);
-    dodger.style.bottom = `${upNum + 5}px`;
+    dodger.style.bottom = `${upNum < 380 ? upNum + 5 : upNum}px`;
 }
 
 function moveDodgerDown(){
     const downPos = dodger.style.bottom.slice(0, -2);
     const downNum = parseInt(downPos, 10);
-    dodger.style.bottom = `${downNum - 5}px`;
+    dodger.style.bottom = `${downNum > 0 ? downNum - 5 : downNum}px`;
 }
 
 document.addEventListener("keydown", (event) => {
